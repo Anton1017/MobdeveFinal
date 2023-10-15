387d6f60baad8a8b6b25f97dc630e62296cb6600
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-15 17:18:23 +0800

    Added sample data but still need the image

---
+++ app/src/main/java/com/example/mobdevemco/DataGenerator.kt
@@ -0,0 +1,16 @@
+package com.example.mobdevemco
+import java.util.ArrayList
+import CustomDate
+class DataGenerator {
+    fun loadEntryData(): ArrayList<Entry>{
+        val data = ArrayList<Entry>()
+        data.add(
+            Entry(
+                "Maria Kart IRL", R.drawable.MariaKart, "Being able to drive the karts like in Mario " +
+                        "Kart reminds me of my Childhood", CustomDate(2023, 0, 10)
+            )
+        )
+
+    }
+
+}
