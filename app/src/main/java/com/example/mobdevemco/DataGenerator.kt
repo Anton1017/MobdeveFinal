b93c13b02fd4d8baceb20a8e43b673915a1aa01b
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-16 21:00:09 +0800

    Another dummy entry added

--- app/src/main/java/com/example/mobdevemco/DataGenerator.kt
+++ app/src/main/java/com/example/mobdevemco/DataGenerator.kt
@@ -6,11 +6,17 @@ class DataGenerator {
         val data = ArrayList<Entry>()
         data.add(
             Entry(
-                "Maria Kart IRL", R.drawable.mariokart, "Being able to drive the karts like in Mario " +
+                "Maria Kart IRL", "Tokyo, Japan", R.drawable.mario_kart, "Being able to drive the karts like in Mario " +
                         "Kart reminds me of my Childhood", CustomDate(2023, 0, 10)
             )
-
         )
+        data.add(
+            Entry(
+                "CAVS vs GSW Game 7", "Tokyo, Japan", R.drawable.mario_kart, "Travelled all the way from the Phillipines to watch Lebron comeback " +
+                        "from a 3-1 deficeit", CustomDate(2016, 5   , 19)
+            )
+        )
+        
         return data
     }
 
