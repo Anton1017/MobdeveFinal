e47438b918c26811190c515c3a84aec04f288f5d
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-10-16 18:16:48 +0800

    update: entry schema addition of location name

--- app/src/main/java/com/example/mobdevemco/Entry.kt
+++ app/src/main/java/com/example/mobdevemco/Entry.kt
@@ -5,6 +5,8 @@ import CustomDate
 class Entry{
     var title: String
         private set
+    var locationName: String
+        private set
     var imageId: Int
         private set
     var description: String
@@ -13,18 +15,20 @@ class Entry{
         private set
 
 
-    constructor(title: String, imageId: Int, description: String, createdAt: CustomDate){
+    constructor(title: String, locationName: String, imageId: Int, description: String, createdAt: CustomDate){
         this.title = title
+        this.locationName = locationName
         this.imageId = imageId
         this.description = description
         this.createdAt = createdAt
     }
 
-    constructor(title: String, imageId: Int, description: String){
+    constructor(title: String, locationName: String, imageId: Int, description: String){
         this.title = title
+        this.locationName = locationName
         this.imageId = imageId
         this.description = description
-        createdAt = CustomDate()
+        this.createdAt = CustomDate()
     }
 
 }
