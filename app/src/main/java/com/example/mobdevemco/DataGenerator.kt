b0561f1583c845aeac0596dcbc1f2a7c1c53364d
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-16 22:07:54 +0800

    Added more data

--- app/src/main/java/com/example/mobdevemco/DataGenerator.kt
+++ app/src/main/java/com/example/mobdevemco/DataGenerator.kt
@@ -6,17 +6,31 @@ class DataGenerator {
         val data = ArrayList<Entry>()
         data.add(
             Entry(
-                "Maria Kart IRL", "Tokyo, Japan", R.drawable.mario_kart, "Being able to drive the karts like in Mario " +
-                        "Kart reminds me of my Childhood", CustomDate(2023, 0, 10)
+                "Maria Kart IRL", "Tokyo, Japan", R.drawable.mario_kart, "Being able to drive the karts like in Mario :)" +
+                        "Kart reminds me of my Childhood", CustomDate(2023, 9, 12)
             )
         )
         data.add(
             Entry(
-                "CAVS vs GSW Game 7", "Tokyo, Japan", R.drawable.mario_kart, "Travelled all the way from the Phillipines to watch Lebron comeback " +
-                        "from a 3-1 deficeit", CustomDate(2016, 5   , 19)
+                "CAVS vs GSW Game 7", "Oracle Arena, California", R.drawable.nba_game, "Travelled all the way from the Phillipines to watch Lebron comeback " +
+                        "from a 3-1 deficeit", CustomDate(2023, 5   , 19)
             )
         )
-        
+        data.add(
+            Entry(
+                "Paris Eiffel Tower Tour", "Paris, France", R.drawable.eiffel_tower, "Exploring the iconic Eiffel Tower in the City of Love.", CustomDate(2022, 11   , 12)
+            )
+        )
+        data.add(
+            Entry(
+                "Beach Paradise in Bali", "Bali, Indonesia", R.drawable.beach_indonesia, "Relaxing on Bali's stunning beaches and enjoy tropical paradise.", CustomDate(2022, 3   , 23)
+            )
+        )
+        data.add(
+            Entry(
+                "Historic Rome Walk", "Rome, Italy", R.drawable.ruins_italy, "Stroll through ancient Roman ruins and landmarks.", CustomDate(2012, 3 , 23)
+            )
+        )
         return data
     }
 
