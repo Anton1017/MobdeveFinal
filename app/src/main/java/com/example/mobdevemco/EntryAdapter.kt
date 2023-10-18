c62b02c69bc03c7b4e2e9b0551b72f03bd983eec
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-10-18 14:42:08 +0800

    add: entry adapter and view holder

---
+++ app/src/main/java/com/example/mobdevemco/EntryAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.mobdevemco
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EntryAdapter(private val data: ArrayList<Entry>) : RecyclerView.Adapter<EntryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: EntryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/mobdevemco/EntryViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.mobdevemco
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobdevemco.databinding.EntryItemBinding
+
+class EntryViewHolder(private val viewBinding: EntryItemBinding): RecyclerView.ViewHolder(viewBinding.root) {
+
+}
