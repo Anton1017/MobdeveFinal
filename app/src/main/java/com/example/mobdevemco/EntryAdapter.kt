57afddde36e951603ab0ba3b401d5848ffa5e4cc
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-10-18 19:05:30 +0800

    update: entry adapter required functions

--- app/src/main/java/com/example/mobdevemco/EntryAdapter.kt
+++ app/src/main/java/com/example/mobdevemco/EntryAdapter.kt
@@ -1,19 +1,30 @@
 package com.example.mobdevemco
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mobdevemco.databinding.EntryItemBinding
 
 class EntryAdapter(private val data: ArrayList<Entry>) : RecyclerView.Adapter<EntryViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
-        TODO("Not yet implemented")
+        // Initialize the ViewBinding of an item's layout
+        val itemViewBinding: EntryItemBinding = EntryItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        val entryViewHolder = EntryViewHolder(itemViewBinding)
+
+
+
+        return entryViewHolder
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return data.size
     }
 
     override fun onBindViewHolder(holder: EntryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindData(data[position])
     }
 
 }
