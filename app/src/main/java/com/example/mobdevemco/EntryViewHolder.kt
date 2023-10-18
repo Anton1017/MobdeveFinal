27c42bca36e4e0a00053b404b2520163dd4e0de2
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-10-18 19:04:45 +0800

    update: entry view holder binding

--- app/src/main/java/com/example/mobdevemco/EntryViewHolder.kt
+++ app/src/main/java/com/example/mobdevemco/EntryViewHolder.kt
@@ -4,5 +4,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mobdevemco.databinding.EntryItemBinding
 
 class EntryViewHolder(private val viewBinding: EntryItemBinding): RecyclerView.ViewHolder(viewBinding.root) {
-
+    fun bindData(entry: Entry) {
+        this.viewBinding.entryTitle.text = entry.title
+        this.viewBinding.entryLocationName.text = entry.locationName
+        // TODO: images for recyclerView
+        this.viewBinding.entryDescription.text = entry.description
+        this.viewBinding.entryCreatedAt.text = entry.createdAt.toStringFull()
+    }
 }
