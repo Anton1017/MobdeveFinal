2fc644a582cf078b89d8a44e1eff34010fb4d439
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-19 23:51:46 +0800

    Added image in viewholder

--- app/src/main/java/com/example/mobdevemco/EntryViewHolder.kt
+++ app/src/main/java/com/example/mobdevemco/EntryViewHolder.kt
@@ -12,6 +12,7 @@ class EntryViewHolder(private val viewBinding: EntryItemBinding): RecyclerView.V
         // TODO: images for recyclerView
         this.viewBinding.entryDescription.text = entry.description
         this.viewBinding.entryCreatedAt.text = entry.createdAt.toStringFull()
+        this.viewBinding.entroImage.setImageResource(entry.imageId)
     }
 
     fun setDeleteOnClickListener(onClickListener: View.OnClickListener){
