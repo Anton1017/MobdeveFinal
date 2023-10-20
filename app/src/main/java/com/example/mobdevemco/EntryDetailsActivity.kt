4ae837bc64472a49f99376296cf380c4a60adfff
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-20 23:04:26 +0800

    Added delete idea in view entry details

--- app/src/main/java/com/example/mobdevemco/EntryDetailsActivity.kt
+++ app/src/main/java/com/example/mobdevemco/EntryDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobdevemco
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobdevemco.databinding.ActivityViewEntryBinding
 
@@ -11,6 +12,20 @@ class EntryDetailsActivity: AppCompatActivity() {
 
         val viewBinding : ActivityViewEntryBinding = ActivityViewEntryBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+        val builder = AlertDialog.Builder(this)
+        viewBinding.deletebtn.setOnClickListener(View.OnClickListener{
+            builder.setTitle("Alert!")
+                .setMessage("Are you sure you want to delete " + viewBinding.entryTitle.text)
+                .setCancelable(true)
+                .setPositiveButton("Yes"){dialogInterface,it ->
+                    finish()
+                }
+                .setNegativeButton("No"){dialogInterface,it ->
+                    dialogInterface.cancel()
+                }
+                .show()
+        })
+
 
     }
 }
