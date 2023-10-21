0b488645ed437cd36261fc03d491984f087af8fd
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2023-10-21 16:32:42 +0800

    Added images in the entry edit

--- app/src/main/java/com/example/mobdevemco/EntryEditActivity.kt
+++ app/src/main/java/com/example/mobdevemco/EntryEditActivity.kt
@@ -8,10 +8,16 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.example.mobdevemco.databinding.ActivityEditEntryBinding
 
 class EntryEditActivity : AppCompatActivity(){
+    private var entryDataImages = DataGenerator.loadEntryImageData()
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var myAdapter: EntryImageAdapter
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         val viewBinding : ActivityEditEntryBinding = ActivityEditEntryBinding.inflate(layoutInflater)
@@ -38,5 +44,16 @@ class EntryEditActivity : AppCompatActivity(){
             this.startActivity(intent)
         })
 
+        this.recyclerView = viewBinding.imageListRecyclerView
+        this.myAdapter = EntryImageAdapter(entryDataImages)
+        this.recyclerView.adapter = myAdapter
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        recyclerView.layoutManager = linearLayoutManager
+
+        //this.viewBinding.entroImage.setImageResource(entry.imageId)
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        recyclerView.setOnFlingListener(null);
+        snapHelper.attachToRecyclerView(recyclerView)
     }
 }
