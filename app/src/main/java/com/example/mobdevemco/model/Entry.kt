32d8ada255977f30a03eee5611d3c56958322aad
rygo0712 <ryan_jefferson_go@dlsu.edu.ph>
2023-11-13 22:25:57 +0800

    update: Entry model update

--- app/src/main/java/com/example/mobdevemco/model/Entry.kt
+++ app/src/main/java/com/example/mobdevemco/model/Entry.kt
@@ -1,7 +1,25 @@
 package com.example.mobdevemco.model
 
 
-class Entry (var title: String, var locationName: String, var images: ArrayList<EntryImages>, var description: String, var createdAt: CustomDate)
+class Entry (var title: String, var locationName: String, var images: ArrayList<EntryImages>, var description: String){
+    var id = 0
+    var createdAt : CustomDate = CustomDate()
+    constructor(title: String, locationName: String, images: ArrayList<EntryImages>, description: String, createdAt: CustomDate) : this(title, locationName, images, description) {
+        this.title = title
+        this.locationName = locationName
+        this.images = images
+        this.description = description
+        this.createdAt = createdAt
+    }
+    constructor(title: String, locationName: String, images: ArrayList<EntryImages>, description: String, createdAt: CustomDate, id: Int) : this(title, locationName, images, description) {
+        this.title = title
+        this.locationName = locationName
+        this.images = images
+        this.description = description
+        this.createdAt = createdAt
+        this.id = id
+    }
+}
 //    var title: String
 //        private set
 //    var locationName: String
